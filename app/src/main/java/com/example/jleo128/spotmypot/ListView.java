e14a64b9c6e2379a3631bd0759800487bf950ead
andy4heels <andrewck@live.unc.edu>
2017-04-30 16:26:22 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/jleo128/spotmypot/ListView.java
@@ -0,0 +1,111 @@
+package edu.unc.andrewck.spotmypot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ListView extends AppCompatActivity implements View.OnClickListener {
+
+    private ArrayList<Bathroom> rList;
+    private LinearLayout l;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        Spinner s = (Spinner) findViewById(R.id.lvSort);
+        ArrayAdapter<CharSequence> a = ArrayAdapter.createFromResource(this,
+                R.array.sort_array, android.R.layout.simple_spinner_item);
+        a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(a);
+
+        l = (LinearLayout) findViewById(R.id.lvList);
+
+        try {
+            rList = (ArrayList) InternalStorage.readObject(this, "list");
+        } catch(IOException e) {}
+        catch(ClassNotFoundException e) {}
+
+        for(Bathroom b : rList) addToList(l, b);
+    }
+
+    protected void sortList(View v) {
+        Spinner s = (Spinner) findViewById(R.id.lvSort);
+
+        String sortBy = s.getSelectedItem().toString();
+
+        switch(sortBy) {
+            case "Name":
+                Collections.sort(rList, new Comparator<Bathroom>() {
+                    @Override
+                    public int compare(Bathroom b2, Bathroom b1)
+                    {
+                        return  b1.getName().compareTo(b2.getName());
+                    }
+                });
+                break;
+            case "Building":
+                Collections.sort(rList, new Comparator<Bathroom>() {
+                    @Override
+                    public int compare(Bathroom b2, Bathroom b1)
+                    {
+                        return  b1.getBuilding().compareTo(b2.getBuilding());
+                    }
+                });
+                break;
+            case "Gender":
+                Collections.sort(rList, new Comparator<Bathroom>() {
+                    @Override
+                    public int compare(Bathroom b2, Bathroom b1)
+                    {
+                        return  b1.getGender().compareTo(b2.getGender());
+                    }
+                });
+                break;
+            case "Rating":
+                Collections.sort(rList, new Comparator<Bathroom>() {
+                    @Override
+                    public int compare(Bathroom b2, Bathroom b1)
+                    {
+                        return  b1.getStars() - b2.getStars();
+                    }
+                });
+                break;
+            default:
+        }
+
+        l.removeAllViews();
+        for(Bathroom b : rList) addToList(l, b);
+    }
+
+    protected void addReview(View v) {
+        Intent i = new Intent(this, MapActivity.class);
+        startActivity(i);
+    }
+
+    private void addToList(LinearLayout l, Bathroom b) {
+        View r = new SmallReview(this, b);
+        r.setOnClickListener(this);
+        l.addView(r, l.getWidth(), 80);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, SingleReview.class);
+        i.putExtra("review", ((SmallReview) v).getReview());
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/jleo128/spotmypot/SmallReview.java
@@ -0,0 +1,55 @@
+package edu.unc.andrewck.spotmypot;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by andrewck on 4/30/17.
+ */
+
+public class SmallReview extends View {
+
+    private Bathroom b;
+
+    public SmallReview(Context context, Bathroom bath) {
+        super(context);
+        b = bath;
+    }
+
+    public SmallReview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmallReview(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SmallReview(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void onDraw(Canvas c) {
+        super.onDraw(c);
+
+        Paint black = new Paint(); black.setColor(Color.BLACK);
+
+        c.drawLine(0, 0, getWidth(), 0, black);
+        c.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1, black);
+
+        black.setTextSize(20);
+
+        c.drawText(b.getName() + " (" + b.getGender() + ")", 10, 30, black);
+
+        black.setTextSize((15));
+
+        c.drawText(b.getBuilding() + ", Floor " + b.getFloor(), 10, 60, black);
+
+        c.drawText(b.getStars() + " Stars", getWidth() - 30, 40, black);
+    }
+
+    public Bathroom getReview() { return b; }
+}
