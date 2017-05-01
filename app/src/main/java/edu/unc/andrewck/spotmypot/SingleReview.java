0b154cf3413d2e7c5b59e42803b8533417a917fb
andy4heels <andrewck@live.unc.edu>
2017-05-01 10:07:05 -0400

    Update SingleReview.java

--- app/src/main/java/edu/unc/andrewck/spotmypot/SingleReview.java
+++ app/src/main/java/edu/unc/andrewck/spotmypot/SingleReview.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -33,13 +36,25 @@ public class SingleReview extends AppCompatActivity implements OnMapReadyCallbac
         ((TextView) findViewById(R.id.srStars)).setText(b.getStars() + " Stars");
         ((TextView) findViewById(R.id.srReview)).setText(b.getReview());
 
-        ((MapView) findViewById(R.id.srMap)).getMapAsync(this);
+        ((MapFragment) getFragmentManager().findFragmentById(R.id.srMap)).getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(b.getLocation()));
-        googleMap.addMarker(new MarkerOptions().position(b.getLocation()));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(b.getLocation(), 17f));
+        switch(b.getGender()) {
+            case "Men's":
+                googleMap.addMarker(new MarkerOptions().position(b.getLocation())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                break;
+            case "Women's":
+                googleMap.addMarker(new MarkerOptions().position(b.getLocation())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+                break;
+            default:
+                googleMap.addMarker(new MarkerOptions().position(b.getLocation())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        }
     }
 
     protected void srReturn(View v) {
@@ -56,18 +71,21 @@ public class SingleReview extends AppCompatActivity implements OnMapReadyCallbac
         }catch(IOException e) {
             reviewList = new ArrayList<Bathroom>();
         }
-        for (Bathroom r : reviewList) {
-            if (r.equals(b))
-            {
-                reviewList.remove(r);
-            }
+
+        Bathroom markForDelete = null;
+        for(Bathroom bath : reviewList) {
+            if(bath.equals(b)) markForDelete = bath;
         }
+        reviewList.remove(markForDelete);
 
         try {
             InternalStorage.writeObject(this, "list", reviewList);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Toast.makeText(this, "Review deleted.", Toast.LENGTH_SHORT).show();
+
         Intent i = new Intent(this, MainMenu.class);
         startActivity(i);
     }
